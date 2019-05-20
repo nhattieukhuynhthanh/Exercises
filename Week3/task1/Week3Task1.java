96473aad3fb67ed7f102dbb501f3699d74cc96c3
Duong Tran Dinh <tranduongthhd@gmail.com>
2019-05-20 10:54:26 +0700

    init

---
+++ Week3/task1/Week3Task1.java
@@ -0,0 +1,13 @@
+package task1;
+
+public class Week3Task1 {
+    // compute greatest common divisor of $a and $b
+    public int GCD(int a, int b) {
+        // return;
+    }
+
+    // compute the fibonaxi of $n
+    public int Fibonaxi(int n) {
+
+    }
+}
---
+++ Week3/task2/Fraction.java
@@ -0,0 +1,41 @@
+package task2;
+
+public class Fraction {
+    /**
+     * declare attributes here
+     */
+
+    /**
+     * declare constructor here
+     */
+    
+    /**
+     * methods
+     */
+    // add
+    public Fraction add(Fraction other) {
+        
+    }
+
+    // subtract
+    public Fraction subtract0(Fraction other) {
+        
+    }
+
+    // multiple
+    public Fraction multiple(Fraction other) {
+        
+    }
+
+    // divide
+    public Fraction divide(Fraction other) {
+        
+    }
+
+    /**
+     * compare this with other, notice that param is Object type
+     */
+    public boolean equals(Object obj) {
+        
+    }
+}
