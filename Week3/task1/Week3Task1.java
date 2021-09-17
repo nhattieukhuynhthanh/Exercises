c7a1653bd60c2a9760b164e907a3ceb354aa30f2
nhattieukhuynhthanh <90900150+nhattieukhuynhthanh@users.noreply.github.com>
2021-09-17 16:15:00 +0700

    Update Week3Task1.java

--- Week3/task1/Week3Task1.java
+++ Week3/task1/Week3Task1.java
@@ -1,23 +1,25 @@
 package task1;
 
 public class Week3Task1 {
-    // compute greatest common divisor of $a and $b
-    public int gcd(int a, int b) {
-        // return;
-    }
-
-    // compute the fibonacci of $n
+    
     public int fibonacci(int n) {
-
+        int f0 = 0;
+        int f1 = 1;
+        int fn = 1;
+ 
+        if (n < 0) {
+            return -1;
+        } else if (n == 0 || n == 1) {
+            return n;
+        } else {
+            for (int i = 2; i < n; i++) {
+                f0 = f1;
+                f1 = fn;
+                fn = f0 + f1;
+            }
+        }
+        return fn;
     }
 
-    /**
-     * print all prime numbers from 0 to $n use Sieve of Eratosthenes algorithm
-     * useful link https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
-     * notice that print each number in a line
-     * @param n
-     */
-    public void sieveEratosthenes(int n) {
-
-    }
+    
 }
